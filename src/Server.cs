6e7ea5a28d263b57f74c378921fde275761b05f7
MykolaKushnarenko <nikokusha@gmail.com>
2025-05-14 22:23:08 +0300

    Remove redundant code

--- src/Server.cs
+++ src/Server.cs
@@ -7,7 +7,6 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
 
-var socket = server.AcceptSocket();
 await socket.SendAsync("+PONG\\r\\n"u8.ToArray());
