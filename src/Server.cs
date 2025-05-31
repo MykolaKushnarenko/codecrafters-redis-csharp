e36215289246351788ce57e31c2a6e097e5a3782
MykolaKushnarenko <nikokusha@gmail.com>
2025-05-31 21:09:33 +0300

    Accept multiple request

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -9,4 +10,15 @@ var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
 
-await socket.SendAsync("+PONG\r\n"u8.ToArray());
+while (true)
+{
+    var buffer = new byte[1024];
+    var received = await socket.ReceiveAsync(buffer, SocketFlags.None);
+    
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+    if (response == "PING")
+    {
+        await socket.SendAsync("+PONG\r\n"u8.ToArray());
+    }
+}
+
