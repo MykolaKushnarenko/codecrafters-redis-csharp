b9ebfaedc38982d0068dc9963130979509d9821a
MykolaKushnarenko <nikokusha@gmail.com>
2025-07-06 14:34:14 +0300

    Add support for storage

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,6 +10,8 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
+
+var storage = new ConcurrentDictionary<string, object>();
 
 const string eom = "\r\n";
 while (true)
@@ -41,6 +44,33 @@ async Task HandleClientRequest(Socket socket)
             var pong = $"${echoResponse.Length}\r\n{echoResponse}\r\n";
             var sendMessageBytes = Encoding.UTF8.GetBytes(pong);
             await socket.SendAsync(sendMessageBytes, SocketFlags.None);
+        }
+        
+        if (result.Name.Equals("SET", StringComparison.CurrentCultureIgnoreCase))
+        {
+            var inputKey = result.Arguments[0].ToString();
+            var inputValue = result.Arguments[1].ToString();
+            storage[inputKey] = inputValue;
+            var sendMessageBytes = "+OK\r\n"u8.ToArray();
+            await socket.SendAsync(sendMessageBytes, SocketFlags.None);
+        }
+        
+        if (result.Name.Equals("GET", StringComparison.CurrentCultureIgnoreCase))
+        {
+            var inputKey = result.Arguments[0].ToString();
+            if(storage.TryGetValue(inputKey, out var value))
+            {
+                var stringValue = value.ToString();
+                var pong = $"${stringValue.Length}\r\n{stringValue}\r\n";
+                var sendMessageBytes = Encoding.UTF8.GetBytes(pong);
+                await socket.SendAsync(sendMessageBytes, SocketFlags.None);
+            }
+            else
+            {
+                var sendMessageBytes = "_\r\n"u8.ToArray();
+                await socket.SendAsync(sendMessageBytes, SocketFlags.None);
+            }
+            
         }
     }
 }
