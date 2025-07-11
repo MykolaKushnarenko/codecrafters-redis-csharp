95c9b9c0e489d094fdb700915d8976982d19402d
MykolaKushnarenko <nikokusha@gmail.com>
2025-07-11 13:29:08 +0300

    Return more info on info command

--- src/BuildingBlocks/Handlers/InfoCommandHandler.cs
+++ src/BuildingBlocks/Handlers/InfoCommandHandler.cs
@@ -21,8 +21,15 @@ public class InfoCommandHandler : ICommandHandler<Command>
 
         if (subCommand.Equals("replication", StringComparison.CurrentCultureIgnoreCase))
         {
-            var roleReply = $"role:{_configuration.Role}";
-            var reply = $"${roleReply.Length}{Constants.EOL}{roleReply}{Constants.EOL}";
+            var roleReply = $"role:{_configuration.Role}{Constants.EOL}";
+            var replicationIdReply = "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"; // for now just hardcode
+            var replicationOffsetReply = $"master_repl_offset:0{Constants.EOL}"; // for now just hardcode
+            var reply =
+                $"${roleReply.Length + replicationIdReply.Length + replicationOffsetReply.Length}{Constants.EOL}" +
+                $"{roleReply}" +
+                $"{replicationOffsetReply}"+
+                $"{replicationIdReply}{Constants.EOL}";
+            
             return Task.FromResult(Encoding.UTF8.GetBytes(reply));
         }
         
